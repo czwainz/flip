7dfa4f08c2cec79d55daf0b556fa389084730de9
Chrissy Zwainz <zchrissychrissy@gmail.com>
2018-12-17 16:58:17 -0700

    auth component issues

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,16 +1,61 @@
 <template>
-  <div class="home">
-
+  <div class="home container-fluid">
+    <div class="row logoRow">
+      <div v-if="showLoginForm()" class="col-12">
+        <auth></auth>
+      </div>
+      <div v-else class="col-12">
+        <img src="../assets/flip-logo.png" class="mainLogo">
+      </div>
+    </div>
+    <div class="row d-flex justify-content-center">
+      <div class="col-12">
+        <!-- CAROUSEL -->
+        <img src="https://placehold.it/200x150" alt="">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <h6>VIEW PUBLIC DECKS</h6>
+      </div>
+      <div class="col-6">
+        <img src="https://placehold.it/100x100" alt="">
+      </div>
+      <div class="col-6">
+        <img src="https://placehold.it/100x100" alt="">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   // @ is an alias to /src
-
+  import auth from '@/components/auth.vue'
 
   export default {
     name: 'home',
     components: {
+      auth
+    },
+    computed: {
+      showLoginForm() {
+        return this.$state.showLoginForm
+      }
     }
   }
 </script>
+
+<style scoped>
+  .mainLogo {
+    width: 80vw;
+    height: auto;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+  }
+
+  .container-fluid {
+    min-height: 100vh;
+    background-color: var(--primary);
+    opacity: .7;
+  }
+</style>
