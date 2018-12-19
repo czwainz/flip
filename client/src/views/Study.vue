7bd92d9e16236a690b7218fc441eca9e8793f002
Cory Brighton <corysairplane@gmail.com>
2018-12-19 16:58:26 -0700

    Flip

--- client/src/views/Study.vue
+++ client/src/views/Study.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="container-fluid">
-    <div class="row studyRow">
-      <div class="flip-card">
-        <div class="flip-card-inner">
-          <div class="flip-card-front">
+    <div class="row studyRow justify-content-center d-flex">
+      <div @click="back = !back" class="flip-card ">
+        <div :class="{'transition-flip': back}" class="flip-card-inner">
+          <div class="flip-card-front justify-content-center d-flex">
             <!-- Front of Card -->
-            <div class="card study">
+            <div class="card study ">
               <h1 class="card-info"> {{activeDeck.cards[0].front}} </h1>
             </div>
           </div>
-          <div class="flip-card-back">
+          <div class="flip-card-back justify-content-center d-flex">
             <!-- Back of Card -->
-            <div class="card study">
+            <div class="card study ">
               <h1 class="card-info"> {{activeDeck.cards[0].back}} </h1>
             </div>
           </div>
@@ -26,7 +26,7 @@
     name: 'study',
     data() {
       return {
-
+        back: false
       }
     },
     computed: {
@@ -60,32 +60,29 @@
   }
 
   .flip-card {
-    width: 98vw;
+    width: 96vw;
     height: 100%;
     perspective: 1000px;
   }
 
   .flip-card-inner {
-    position: relative;
-    text-align: center;
-    transition: transform 0.6s;
+    transition: transform 1.5s;
     transform-style: preserve-3d;
   }
 
-  .flip-card:hover .flip-card-inner {
+  .transition-flip {
     transform: rotateY(180deg);
   }
 
   .flip-card-front,
   .flip-card-back {
     position: absolute;
-    width: 94vw;
+    width: 96vw;
     height: 94vh;
     backface-visibility: hidden;
   }
 
   .flip-card-front {
-    /* background-color: #bbb; */
     z-index: 2;
   }
 
