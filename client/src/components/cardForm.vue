a8f89b08366e55a693979e1109c3228a45333be2
Chrissy Zwainz <zchrissychrissy@gmail.com>
2018-12-20 09:26:25 -0700

    Moving around edit card button

--- client/src/components/cardForm.vue
+++ client/src/components/cardForm.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="cardForm">
     <div class="row">
-      <form @submit.prevent="addCard">
-        <div class="form-group">
-          <label for="cardFront">Card Front</label>
-          <input type="text" class="form-control" name="cardFront" placeholder="Card Front" maxlength="250">
-          <label for="cardBack">Card Back</label>
-          <input type="text" class="form-control" name="cardBack" placeholder="Card Back" maxlength="250">
-        </div>
-      </form>
+      <div class="col-12">
+
+        <form @submit.prevent="addCard">
+          <div class="form-group">
+            <label for="cardFront">Card Front</label>
+            <input type="text" class="form-control" name="cardFront" placeholder="Card Front" maxlength="250">
+            <label for="cardBack">Card Back</label>
+            <input type="text" class="form-control" name="cardBack" placeholder="Card Back" maxlength="250">
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
