f74be6a6bab4b7261e2a783daa7c0b62f2ec52e4
Chrissy Zwainz <zchrissychrissy@gmail.com>
2019-01-02 10:00:57 -0700

    public pages

--- client/src/components/myDeckPages.vue
+++ client/src/components/myDeckPages.vue
@@ -5,7 +5,13 @@
       <strong>{{deck.title}}</strong>
       <i>{{deck.description}}</i>
     </div>
-  
+    <div class="col-12 d-flex justify-content-around mt-4 mb-5 mr-5 ml-5">
+      <button :disabled="!showPrevLink()" type="button" class="btn btn-secondary btn-sm shadow-sm" @click="updatePage(currentPage-1)"><i
+          class="fas fa-angle-double-left"></i></button>
+      Page {{currentPage+1}} of {{totalPages()}}
+      <button :disabled="!showNextLink()" type="button" class="btn btn-secondary btn-sm shadow-sm" @click="updatePage(currentPage+1)"><i
+          class="fas fa-angle-double-right"></i></button>
+    </div>
   </div>
 </template>
 
@@ -34,6 +40,23 @@
     methods: {
       goToDeckView(deckId) {
         this.$store.dispatch('getActiveDeck', deckId)
+      },
+      //PAGINATION
+      updateVisibleDecks() {
+        this.visibleDecks = this.myDecks.slice(this.currentPage * this.pageSize, (this.currentPage * this.pageSize) + this.pageSize)
+      },
+      totalPages() {
+        return Math.ceil(this.myDecks.length / this.pageSize)
+      },
+      showPrevLink() {
+        return this.currentPage == 0 ? false : true;
+      },
+      showNextLink() {
+        return this.currentPage == (this.totalPages() - 1) ? false : true
+      },
+      updatePage(pageNumber) {
+        this.currentPage = pageNumber
+        this.updateVisibleDecks()
       }
     }
   }
--- client/src/components/publicPages.vue
+++ client/src/components/publicPages.vue
@@ -22,7 +22,7 @@
 </template>
 <script>
   export default {
-    name: 'pages',
+    name: 'publicPages',
     data() {
       return {
         currentPage: 0,
