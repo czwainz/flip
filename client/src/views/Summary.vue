3d7e50fff16814d6e4f8d62a8526f05e3d453c33
Cory Brighton <corysairplane@gmail.com>
2019-01-11 10:21:36 -0700

    reviews bug removed

--- client/src/views/Summary.vue
+++ client/src/views/Summary.vue
@@ -14,19 +14,19 @@
       <!-- STAR RATING -->
       <form class="col-12 justify-content-center d-flex mt-3">
         <div class="rate">
-          <input type="radio" id="star5" name="rate" value="5" :checked="checkRating(5)" />
+          <input type="radio" id="star5" name="myRating" value="5" v-model="myRating" />
           <label @click="rate(5)" for="star5" title="text">5 stars</label>
 
-          <input type="radio" id="star4" name="rate" value="4" :checked="checkRating(4)" />
+          <input type="radio" id="star4" name="myRating" value="4" v-model="myRating" />
           <label @click="rate(4)" for="star4" title="text">4 stars</label>
 
-          <input type="radio" id="star3" name="rate" value="3" :checked="checkRating(3)" />
+          <input type="radio" id="star3" name="myRating" value="3" v-model="myRating" />
           <label @click="rate(3)" for="star3" title="text">3 stars</label>
 
-          <input type="radio" id="star2" name="rate" value="2" :checked="checkRating(2)" />
+          <input type="radio" id="star2" name="myRating" value="2" v-model="myRating" />
           <label @click="rate(2)" for="star2" title="text">2 stars</label>
 
-          <input type="radio" id="star1" name="rate" value="1" :checked="checkRating(1)" />
+          <input type="radio" id="star1" name="myRating" value="1" v-model="myRating" />
           <label @click="rate(1)" for="star1" title="text">1 star</label>
         </div>
       </form>
@@ -65,8 +65,11 @@
     name: 'summary',
     data() {
       return {
-        myRating: 0
+        myRating: 3
       }
+    },
+    mounted() {
+      this.userHasRating()
     },
     computed: {
       activeDeck() {
